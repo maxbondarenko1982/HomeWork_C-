d3172dd78420d4562c0144bb0dd6aa8755bb3330
maxbondarenko <bondarenkomm@bk.ru>
2022-11-14 23:57:09 +0500

    zadacha02

---
+++ zadacha02/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа 
+и выдаёт, какое число большее, а какое меньшее.*/
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.WriteLine("Первое число больше");
+else
+    Console.WriteLine("Второе число больше");
